1c225d5f5b55f4c39fe3d0e00c0fbd223b05c015
Your Name <ee.marishkin@yandex.ru>
2025-06-08 20:30:26 +0400

    ...

--- src/pages/LifePage.tsx
+++ src/pages/LifePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import '../styles/LifePage.css';
@@ -30,7 +30,7 @@ function LifePage() {
 
   const handleDateChange = (
     value: Date | [Date | null, Date | null] | null,
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    
   ) => {
     if (value instanceof Date) {
       setDate(value);
